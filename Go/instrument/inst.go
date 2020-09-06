05740aa7ecd3e7a4e4ebcc851c8f02dd446cbb69
Florian <f.obersteiner@posteo.de>
2020-09-06 12:31:33 +0200

    inst: added payload map and import options for json and yaml cfg files

--- Go/instrument/inst.go
+++ Go/instrument/inst.go
@@ -6,18 +6,22 @@ package inst
 
 import (
 	"encoding/binary"
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"net"
+	"os"
 	"strconv"
 	"strings"
+
+	"gopkg.in/yaml.v2"
 )
 
 // Instrument - a struct to characterize an instrument in the Conatiner-Lab.
 type Instrument struct {
-	ID        int
-	Name      string
-	Address   string
-	WUallowed bool
+	ID        int    `json:"ID" yaml:"ID"`
+	Name      string `json:"Name" yaml:"Name"`
+	Address   string `json:"Address" yaml:"Address"`
+	WUallowed bool   `json:"WU_allowed" yaml:"WU_allowed"`
 	State     string
 }
 
@@ -32,17 +36,64 @@ func NewInstr() Instrument {
 	return i
 }
 
+// Payload - a mapping of instrument-ID (int) -> instrument type (struct).
+type Payload map[int]Instrument
+
+type instruments struct {
+	Instruments []Instrument `json:"Payload" yaml:"Payload"`
+}
+
+// PayloadFromJSON - fill the payload map with instruments from a config file in json format.
+func PayloadFromJSON(src string) (Payload, error) {
+	var p = Payload{}
+	var inst instruments
+
+	jsonFile, err := os.Open(src)
+	if err != nil {
+		return p, err
+	}
+	defer jsonFile.Close()
+	jsonData, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return p, err
+	}
+
+	json.Unmarshal(jsonData, &inst)
+	for i := 0; i < len(inst.Instruments); i++ {
+		p[inst.Instruments[i].ID] = inst.Instruments[i]
+	}
+	return p, nil
+}
+
+// PayloadFromYAML - fill the payload map with instruments from a config file in yaml format.
+func PayloadFromYAML(src string) (Payload, error) {
+	var p = Payload{}
+	var inst instruments
+
+	yamlFile, err := os.Open(src)
+	if err != nil {
+		return p, err
+	}
+	defer yamlFile.Close()
+	yamlData, err := ioutil.ReadAll(yamlFile)
+	if err != nil {
+		return p, err
+	}
+	yaml.Unmarshal(yamlData, &inst)
+	for i := 0; i < len(inst.Instruments); i++ {
+		p[inst.Instruments[i].ID] = inst.Instruments[i]
+	}
+	return p, nil
+}
+
 // GetAddressBytes - method to get 6-byte array that represents IP address (4 bytes)
 // and UPD port (2 bytes). Address must be of type string with format "x.x.x.x:port"
 func (i Instrument) GetAddressBytes() ([6]byte, bool) {
-
 	parts := strings.Split(i.Address, ":")
 	result := [6]byte{}
-
 	if len(parts) != 2 {
 		return result, false
 	}
-
 	port, err := strconv.Atoi(parts[1])
 	if err != nil {
 		return result, false
--- Go/instrument/inst_test.go
+++ Go/instrument/inst_test.go
@@ -1,16 +1,45 @@
 package inst_test
 
-func main() {
+import (
+	. "car3-master/Go/instrument"
+	"fmt"
+	"net"
+	"os"
+	"path"
+	"testing"
+)
+
+func TestInst(t *testing.T) {
 	inst64 := NewInstr()
 	fmt.Println("default instrument repr:", inst64)
+	b, ok := inst64.GetAddressBytes()
+	fmt.Println(b, ok)
 
 	inst64.ID = 64
 	inst64.Name = "Test_Instr_at_64"
 	inst64.Address = "192.168.1.64:16064"
 	inst64.State = "IN"
 	fmt.Println("inst64 repr:", inst64)
-
 	inst64UDPAddr, _ := net.ResolveUDPAddr("udp", inst64.Address)
 	fmt.Printf("inst64 ip resolved: %T -> %v\n", inst64UDPAddr, inst64UDPAddr)
 
+	b, ok = inst64.GetAddressBytes()
+	fmt.Println(b, ok)
+}
+
+func TestPayloadFromYAML(t *testing.T) {
+	wd, _ := os.Getwd()
+	src := path.Join(wd, "instr_cfg_test.yml")
+	p, _ := PayloadFromYAML(src)
+	for k, v := range p {
+		fmt.Println(k, v)
+	}
+}
+func TestPayloadFromJSON(t *testing.T) {
+	wd, _ := os.Getwd()
+	src := path.Join(wd, "instr_cfg_test.json")
+	p, _ := PayloadFromJSON(src)
+	for k, v := range p {
+		fmt.Println(k, v)
+	}
 }
